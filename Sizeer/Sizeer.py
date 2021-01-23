cf5a744f16dfa8fefa22761b16b64c5c08341c1c
Adi <noodlesthethird23@gmail.com>
2021-01-23 20:21:03 +0200

    fixed

--- Sizeer/Sizeer.py
+++ Sizeer/Sizeer.py
@@ -10,7 +10,7 @@ import urllib3
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, HardwareType
 
-logging.basicConfig(filename='RapCitylog.log', filemode='a', format='%(asctime)s - %(name)s - %(message)s',
+logging.basicConfig(filename='Tikelog.log', filemode='a', format='%(asctime)s - %(name)s - %(message)s',
                     level=logging.DEBUG)
 
 software_names = [SoftwareName.CHROME.value]
@@ -25,27 +25,23 @@ def scrape_main_site(headers):
     :return:
     """
     items = []
-    url = 'https://www.rapcity.ro/ujdonsagok?ccsop=CIP'
+    url = 'https://sizeer.ro/noutati/categorie-incaltaminte'
     s = requests.Session()
     html = s.get(url=url, headers=headers, verify=False, timeout=15)
     soup = BeautifulSoup(html.text, 'html.parser')
-    itemsName = soup.find_all('h2',{'class':'cl-item-title'})
-    itemsColor = soup.find_all('div',{'class':'cl-item-color'})
-    itemsRedirect = soup.find_all('a',{'class':'cl-item col-xs-6 col-sm-4 col-md-3'})
-    itemsImage = soup.find_all('div',{'class':'cl-item-overlay'})
-    itemsPrice = soup.find_all('span',{'class':'price-normal'})
-    print(itemsImage)
-    for i in range(12):
-        itemsName[i]=itemsName[i].text
-        itemsRedirect[i]=itemsRedirect[i]['href']
-        itemsImage[i]=itemsImage[i].find('img')
-        print(itemsImage[i])
+    itemsName = soup.find_all('div',{'class':'b-itemList_item js-offer-variants offer_830257628 js-offerItem'})
+    itemsRedirect = soup.find_all('a',{'class':'b-itemList_photoLink'})
+    itemsImage = soup.find_all('img',{'class':'b-itemList_photoMain b-lazy js-offer-photo b-loaded'})
+    itemsPrice = soup.find_all('p',{'class':'b-itemList_price'})
+    print(itemsName)
+    for i in range(48):
+        itemsName[i]=itemsName[i]['data-ga-name']
+        itemsRedirect[i]=f'https://sizeer.ro{itemsRedirect[i]["href"]}'
         itemsImage[i]=itemsImage[i]['src']
-        itemsImage[i]=f'https://www.rapcity.ro{itemsImage[i]}'
+        itemsImage[i]=f'https://sizeer.ro{itemsImage[i]}'
         itemsPrice[i]=itemsPrice[i].text
-        itemsColor[i]=itemsColor[i].text
-        print(f'Brand: {itemsName[i]}\nLink: {itemsRedirect[i]}\nImagine: {itemsImage[i]}\nPret: {itemsPrice[i]}\nCuloare: {itemsColor[i]}')
-        item = [itemsName[i],itemsRedirect[i],itemsImage[i],itemsPrice[i],itemsColor[i]]
+        print(f'Brand: {itemsName[i]}\nLink: {itemsRedirect[i]}\nImagine: {itemsImage[i]}\nPret: {itemsPrice[i]}')
+        item = [itemsName[i],itemsRedirect[i],itemsImage[i],itemsPrice[i]]
         items.append(item)
     return items
 
@@ -62,10 +58,10 @@ def discord_webhook(product_item):
     data["avatar_url"] = CONFIG['AVATAR_URL']
     data["embeds"] = []
     embed = {}
-    embed["title"] = f"{product_item[0]}"  # Nume produs
-    embed["description"] = f"**Pret:**\n{product_item[3]}\n**Culoare:**{product_item[4]}" # Model produs
-    embed['url'] = f'{product_item[1]}'  # Link produs
-    embed["thumbnail"] = {'url': product_item[2]}  # Imagine produs
+    embed["title"] = f"{product_item[0]} {product_item[1]}"  # Nume produs
+    embed["description"] = f"**Pret:**\n{product_item[4]}" # Model produs
+    embed['url'] = f'{product_item[2]}'  # Link produs
+    embed["thumbnail"] = {'url': product_item[3]}  # Imagine produs
     embed["color"] = int(CONFIG['COLOUR'])
     embed["footer"] = {'text': 'Powered by Busta Romania.'}
     embed["timestamp"] = str(datetime.datetime.utcnow())
@@ -83,11 +79,11 @@ def discord_webhook(product_item):
         logging.info("Mesaj trimis cu succes, code {}.".format(result.status_code))
 
 def checkItems(items):
-    for i in range(0,12):
+    for i in range(0,48):
         if not items[i] in lastItems:
             time.sleep(0.5)
             discord_webhook(items[i])
-    for i in range(0,12):
+    for i in range(0,48):
         lastItems[i] = items[i]
 
 
